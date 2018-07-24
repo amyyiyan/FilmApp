f6bcf2ee6157511d7d6a29e2453ad0f0e933fae6
Amy Liu <amytheliu13@gmail.com>
2018-07-24 14:10:57 -0700

    Initial Commit

---
+++ FilmApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FilmApp
+//
+//  Created by Amy Liu on 7/24/18.
+//  Copyright © 2018 Amy Liu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
